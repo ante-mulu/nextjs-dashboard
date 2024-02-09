19ff28942810da74d7745c3566b12c337caa0122
anteneh <anteneh.mulu1921@gmail.com>
2024-02-09 17:44:58 +0300

    adding error message to other form

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -10,8 +10,12 @@ const FormSchema = z.object({
     invalid_type_error: 'please select a customer',
   }),
   
-  amount: z.coerce.number().gt(0,{message:"please enter an amount greater than $0"}),
-  status: z.enum(['pending', 'paid'],{invalid_type_error:'please select an invoice status.',}),
+  amount: z.coerce
+  .number()
+  .gt(0,{message:"please enter an amount greater than $0"}),
+  status: z.enum(['pending', 'paid'],{
+    invalid_type_error:'please select an invoice status.',
+  }),
   date: z.string(),
 });
 export type State = {
